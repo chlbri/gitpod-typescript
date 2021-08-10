5b17e60e51db624eb5fa2d2a92548070f9892003
chlbri <51156116+chlbri@users.noreply.github.com>
2021-08-10 11:57:51 +0000

    Update bash.sh

--- .gitpod-config/bash.sh
+++ .gitpod-config/bash.sh
@@ -1,2 +1,3 @@
 npm install -g ts-node
-yarn add -D typescript prettier jest-cucumber eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser shelljs edit-json-file
+yarn add -D typescript prettier jest @types/jest ts-jest jest-cucumber eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser shelljs edit-json-file
+yarn add dequal
