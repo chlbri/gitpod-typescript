25bfc2662c4d78030c9bd4212a483f314b35d252
chlbri <51156116+chlbri@users.noreply.github.com>
2022-02-13 14:40:28 +0100

    Update git_branches.sh

--- .gitpod-config/git_branches.sh
+++ .gitpod-config/git_branches.sh
@@ -1,8 +1,8 @@
-gp sync-await build
-git checkout -b main
-git push -u origin main
-git checkout -b staging
-git push -u origin staging
-git checkout -b upgrade
-git push -u origin upgrade
+gp sync-await build ;
+git checkout -b main ;
+git push -u origin main ;
+git checkout -b staging ;
+git push -u origin staging ;
+git checkout -b upgrade ;
+git push -u origin upgrade ;
 git checkout main
